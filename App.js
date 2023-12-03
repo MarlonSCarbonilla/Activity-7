a52c5dc2b017c227633a925270f5d54256ee4e5c
Marlon Carbonilla <111182211+MarlonSCarbonilla@users.noreply.github.com>
2023-12-04 05:39:01 +0800

    Update App.js

--- App.js
+++ App.js
@@ -4,29 +4,28 @@ import Viewdata from './STDNT/Viewdata';
 import Adddata from './STDNT/Adddata';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
+
 const Stack = createStackNavigator();
+
 export default function App() {
   return (
     <NavigationContainer>
-
-     <Stack.Navigator>
-      <Stack.Screen name="Adddata" component={ViewData}  options={{ headerShown: false }}  />
-      <Stack.Screen name="Viewdata" component={ViewData}  options={{ headerShown: false }}  />
-
-    </Stack.Navigator>
+      <Stack.Navigator>
+        <Stack.Screen name="Adddata" component={Adddata} options={{ headerShown: false }} />
+        <Stack.Screen name="Viewdata" component={Viewdata} options={{ headerShown: false }} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
-
 const styles = StyleSheet.create({
   container: {
-  flex: 1,
-  alignContent: 'center',
-   justifyContent: 'center',
-    backgroundColor: 'white'
-    },
-  input:{
-      width: '100%'
-  }
+    flex: 1,
+    alignContent: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'white',
+  },
+  input: {
+    width: '100%',
+  },
 });
